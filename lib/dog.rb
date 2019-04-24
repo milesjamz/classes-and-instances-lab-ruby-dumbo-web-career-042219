8a0e6125b2c1b9c2ce8d2d75cee88ba0b498a0a7
Miles Marillo <mjm03@hampshire.edu>
2019-04-24 01:27:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+  puts "I love to chase cars."
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  puts "I love to drive cars."
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
